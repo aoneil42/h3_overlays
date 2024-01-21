24f84323748f70df09abd94e120e4734dcddc600
Austin Stevens <geoaustin@gmail.com>
2024-01-21 12:58:10 -0700

    Update processcchex.py

--- processcchex.py
+++ processcchex.py
@@ -1,1 +1,8 @@
+import geopandas as gpd
 
+h3_0 = gpd.read_parquet('h3_0.parquet')
+lsib = gpd.read_parquet('lsib.parquet')
+
+lsibhex = gpd.overlay(lsib, h3_0, 'union', False, True)
+
+lsibhex.to_parquet('lsibhex.parquet')
